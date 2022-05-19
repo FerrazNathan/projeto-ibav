55eb51d2fb94ec286efba7d86015792b4427f0bf
Nathan Ferraz <nathanferraz@hotmail.com>
2022-05-18 22:12:10 -0300

    melhorando a visualização na versão mobile

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -8,43 +8,40 @@ import { FaRegCalendarAlt } from 'react-icons/fa'
 import { BiTime } from 'react-icons/bi'
 import { VscLocation } from 'react-icons/vsc'
 
-
-
-
 const About = () => {
-    return (
-        <>
-            <Header about={true} />
-            <Container>
-                <DivGeneral>
-                    <DivInfo>
-                        <FaRegCalendarAlt className="Icon" />
-                        <div>
-                            <h1>Data</h1>
-                            <h2>30/07/2022</h2>
-                        </div>
-                    </DivInfo>
-                    <DivInfo>
-                        <BiTime className="Icon" />
-                        <div>
-                            <h1>horário</h1>
-                            <h2>09:00 ás 20:00</h2>
-                        </div>
-                    </DivInfo>
-                    <DivInfo>
-                        <VscLocation className="Icon" />
-                        <div>
-                            <h1>local</h1>
-                            <h2>ibav Ribeirão Pires</h2>
-                        </div>
-                    </DivInfo>
-                </DivGeneral>
-                <ContainerRight>
-                    <SignIn />
-                </ContainerRight>
-            </Container>
-        </>
-    )
+	return (
+		<>
+			<Header about={true} />
+			<Container>
+				<DivGeneral>
+					<DivInfo>
+						<FaRegCalendarAlt className="Icon" />
+						<div>
+							<h1>Data</h1>
+							<h2>30/07/2022</h2>
+						</div>
+					</DivInfo>
+					<DivInfo>
+						<BiTime className="Icon" />
+						<div>
+							<h1>horário</h1>
+							<h2>09:00 ás 22:00</h2>
+						</div>
+					</DivInfo>
+					<DivInfo>
+						<VscLocation className="Icon" />
+						<div>
+							<h1>local</h1>
+							<h2>ibav Ribeirão Pires</h2>
+						</div>
+					</DivInfo>
+				</DivGeneral>
+				<ContainerRight>
+					<SignIn />
+				</ContainerRight>
+			</Container>
+		</>
+	)
 }
 
 export default About;
